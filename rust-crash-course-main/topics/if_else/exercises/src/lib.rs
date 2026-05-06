bffb680d836d943cf04f86882e8f2a259a27f65b
Armughan Mehmood <armughanofficial20@gmail.com>
2026-05-07 02:49:42 +0500

    Complete Structs, Vectors, HashMaps, and If/Else modules - 37%+ milestone

--- rust-crash-course-main/topics/if_else/exercises/src/lib.rs
+++ rust-crash-course-main/topics/if_else/exercises/src/lib.rs
@@ -1,11 +1,44 @@
 pub fn min(x: i32, y: i32) -> i32 {
-    todo!();
+    
+    if x < y {
+        println!("x is less than y");
+        x
+    }
+    else if x > y {
+        println!("x is greater than y");
+        y
+    }
+    else {
+        println!("x is equal to y");
+        x
+    }
 }
 
 pub fn max(x: i32, y: i32) -> i32 {
-    todo!();
+    
+   
+    if x < y {
+        println!("x is less than y");
+        y
+    }
+    else if x > y {
+        println!("x is greater than y");
+        x
+    }
+    else {
+        println!("x is equal to y");
+        x
+    }
 }
 
 pub fn sign(x: i32) -> i32 {
-    todo!();
+    
+    if x < 0 {    
+        -1
+    }
+    else {
+       1
+    }
+
 }
+
